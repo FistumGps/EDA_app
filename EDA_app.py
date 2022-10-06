14266e8a2080d480a1f1930d41732be33c5fe6d3
FistumGps <fistumgps@gmail.com>
2022-10-06 14:29:35 +0300

    second commit

--- EDA_app.py
+++ EDA_app.py
@@ -4,6 +4,10 @@ import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
+st.set_page_config(page_title= "EDA Report Web App", page_icon=":bar_chart:" ,layout='wide')
+#st.header("Twitter")
+@st.cache
+
 # Web App Title
 st.markdown('''
 # *The EDA App Made By Fistum*
